00e5c368df600c1ca529a1061208be98094d6e89
xKxAxKx <pekepooooong@gmail.com>
2019-01-27 18:30:46 +0900

    users.js

--- app/store/users.js
+++ app/store/users.js
@@ -3,17 +3,17 @@ export const state = () => ({
 })
 
 export const getters = {
-  users: (state) => state.users
+  users: state => state.users
 }
 
 export const mutations = {
   addUser(state, { user }) {
-    state.user.push(user)
+    state.users.push(user)
   },
-  addUserPost(state, {user, post}) {
+  addUserPost(state, { user, post }) {
     state.userPosts[user.id].push(post)
   },
-  createUserPosts(state, { user }) {
+  clearUserPosts(state, { user }) {
     state.userPosts[user.id] = []
   }
 }
@@ -22,5 +22,19 @@ export const actions = {
   async fetchUser({ commit }, { id }) {
     const user = await this.$axios.$get(`/users/${id}.json`)
     commit('addUser', { user })
+  },
+  async addLikeLogToUser({ commit }, { user, post }) {
+    post.likes.push({
+      created_at: moment().format(),
+      user_id: user.id,
+      post_id: post.id
+    })
+    const newPost = await this.$axios.$put(`/posts/${post.id}.json`, post)
+    commit('updatePost', { post: newPost })
+  },
+  async removeLikeLogToUser({ commit }, { user, post }) {
+    post.likes = post.likes.filter(like => like.user_id !== user.id) || []
+    const newPost = await this.$axios.$put(`/posts/${post.id}.json`, post)
+    commit('updatePost', { post: newPost })
   }
 }
