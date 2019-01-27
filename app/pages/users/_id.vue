e00243891696b39be3887b14a72c516435bfefde
xKxAxKx <pekepooooong@gmail.com>
2019-01-27 18:50:18 +0900

    fumuuu

--- app/pages/users/_id.vue
+++ app/pages/users/_id.vue
@@ -1,50 +1,49 @@
 <template>
   <div>
-    <el-row v-if="user">
-      <el-col :span="6">
-        <el-card class="text-center" style="margin-right: 16px;">
-          <div>
-            <img
-              src="https://placehold.it/150x150"
-              style="width: 100%;margin-bottom: 16px;border-radius:2px;"
-              alt=""
-            />
-          </div>
-          <h2>
-            <b>{{user.id}}</b>
-          </h2>
-        </el-card>
-      </el-col>
-      <el-col :span="18">
-        <el-card>
-          <div slot="header" class="clearfix">
-            <span>{{user.id}} さんの投稿</span>
-          </div>
-          <el-table
-            :data="userPosts"
-            style="width: 100%"
-            class="table"
-          >
-            <el-table-column
-              prop="title"
-              label="タイトル"
-            />
-            <el-table-column
-              prop="created_at"
-              label="投稿日時"
-              width="160"
-            />
-          </el-table>
-        </el-card>
-      </el-col>
-    </el-row>
+      <el-row v-if="user">
+        <el-col :span="6">
+          <el-card class="text-center" style="margin-right: 16px;">
+            <div>
+              <img
+                src="https://placehold.it/150x150"
+                style="width: 100%;margin-bottom: 16px;border-radius:2px;"
+                alt=""
+              />
+            </div>
+            <h2>
+              <b>{{user.id}}</b>
+            </h2>
+          </el-card>
+        </el-col>
+        <el-col :span="18">
+          <el-card>
+            <div slot="header" class="clearfix">
+              <span>{{user.id}} さんの投稿</span>
+            </div>
+            <el-table
+              :data="userPosts"
+              style="width: 100%"
+              class="table"
+            >
+              <el-table-column
+                prop="title"
+                label="タイトル"
+              />
+              <el-table-column
+                prop="created_at"
+                label="投稿日時"
+                width="160"
+              />
+            </el-table>
+          </el-card>
+        </el-col>
+      </el-row>
   </div>
 </template>
 
 <script>
 import moment from '~/plugins/moment'
 import { mapGetters } from 'vuex'
-
 export default {
   async asyncData({ store, route, error }) {
     const { id } = route.params
