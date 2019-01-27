0efe13296702609cbc6a01e2bc013fa27e246dbf
xKxAxKx <pekepooooong@gmail.com>
2019-01-27 18:28:42 +0900

    posts.jsにremoveLikeとかその他

--- app/store/posts.js
+++ app/store/posts.js
@@ -23,12 +23,12 @@ export const mutations = {
 export const actions = {
   async fetchPost({ commit }, { id }) {
     const post = await this.$axios.$get(`/posts/${id}.json`)
-    commit('addPost', { post: { ...post, id }})
+    commit('addPost', { post: { ...post, id } })
   },
   async fetchPosts({ commit }) {
     const posts = await this.$axios.$get(`/posts.json`)
     commit('clearPosts')
-    Object.entries(posts)
+    Object.entries(posts || [])
       .reverse()
       .forEach(([id, content]) =>
         commit('addPost', {
@@ -41,14 +41,18 @@ export const actions = {
   },
   async publishPost({ commit }, { payload }) {
     const user = await this.$axios.$get(`/users/${payload.user.id}.json`)
-    const post_id = (await this.$axios.$post('/posts.json', payload)).name
     const created_at = moment().format()
-    const post = { id: post_id, ...payload, created_at}
-    const pubData = { id: post_id, ...payload, created_at}
-    delete pubData.user
+    payload = {
+      created_at,
+      ...payload
+    }
+    const post_id = (await this.$axios.$post('/posts.json', payload)).name
+    const post = { id: post_id, ...payload, created_at }
+    const putData = { id: post_id, ...payload, created_at }
+    delete putData.user
     await this.$axios.$put(`/users/${user.id}/posts.json`, [
       ...(user.posts || []),
-      pubData
+      putData
     ])
     commit('addPost', { post })
   },
@@ -58,6 +62,11 @@ export const actions = {
       user_id: user.id,
       post_id: post.id
     })
+    const newPost = await this.$axios.$put(`/posts/${post.id}.json`, post)
+    commit('updatePost', { post: newPost })
+  },
+  async removeLikeToPost({ commit }, { user, post }) {
+    post.likes = post.likes.filter(like => like.user_id !== user.id) || []
     const newPost = await this.$axios.$put(`/posts/${post.id}.json`, post)
     commit('updatePost', { post: newPost })
   }
