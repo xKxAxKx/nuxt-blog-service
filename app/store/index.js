a4a13ceb9d09e7d8853a988684f9145880dbd51d
xKxAxKx <pekepooooong@gmail.com>
2019-01-27 18:30:03 +0900

    update:index.js

--- app/store/index.js
+++ app/store/index.js
@@ -6,22 +6,32 @@ export const state = () => ({
 })
 
 export const getters = {
-  isLoggedIn: (state) => state.isLoggedIn,
+  isLoggedIn: state => state.isLoggedIn,
   user: state => (state.user ? Object.assign({ likes: [] }, state.user) : null)
 }
 
 export const mutations = {
   setUser(state, { user }) {
+    if (user.id.match(/_|@|\./)) {
+      throw new TypeError('invalid username')
+    }
     state.user = user
     state.isLoggedIn = true
+  },
+  updateUser(state, { user }) {
+    state.user = user
   }
 }
 
 export const actions = {
   async login({ commit }, { id }) {
+    if (id.match(/_|@|\./)) {
+      throw new TypeError('invalid username')
+    }
     const user = await this.$axios.$get(`/users/${id}.json`)
-    if (!user.id) throw new Error('invalid user')
-    commit('setUser', {user})
+    console.log(user)
+    if (!user.id) throw new Error('Invalid user')
+    commit('setUser', { user })
   },
   async register({ commit }, { id }) {
     const payload = {}
@@ -29,7 +39,7 @@ export const actions = {
     await this.$axios.$patch(`/users.json`, payload)
     const user = await this.$axios.$get(`/users/${id}.json`)
     if (!user.id) throw new Error('Invalid user')
-    commit('setUser', {user})
+    commit('setUser', { user })
   },
   async addLikeLogToUser({ commit }, { user, post }) {
     user.likes.push({
@@ -38,6 +48,11 @@ export const actions = {
       post_id: post.id
     })
     const newUser = await this.$axios.$put(`/users/${user.id}.json`, user)
-    commit('setUser', { user: newUser })
+    commit('updateUser', { user: newUser })
+  },
+  async removeLikeLogToUser({ commit }, { user, post }) {
+    user.likes = post.likes.filter(like => like.user_id !== user.id) || []
+    const newUser = await this.$axios.$put(`/users/${user.id}.json`, user)
+    commit('updateUser', { user: newUser })
   }
 }
